82f018f2d2571a2ecf9ebd9d7e8ec0048197f577
soukupa <soukup.andrej@gmail.com>
2023-03-06 21:37:13 +0100

    initial commit 2

--- src/main/java/sk/soukupandrej/loans/controller/LoansController.java
+++ src/main/java/sk/soukupandrej/loans/controller/LoansController.java
@@ -1,12 +1,18 @@
 package sk.soukupandrej.loans.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sk.soukupandrej.loans.config.LoansConfigService;
 import sk.soukupandrej.loans.model.Customer;
 import sk.soukupandrej.loans.model.Loans;
+import sk.soukupandrej.loans.model.Properties;
 import sk.soukupandrej.loans.repository.LoansRepository;
 
 import java.util.List;
@@ -16,8 +22,19 @@ public class LoansController {
     @Autowired
     private LoansRepository loansRepository;
 
+    @Autowired
+    private LoansConfigService loansConfig;
+
     @PostMapping("/myLoans")
     public List<Loans> getLoansDetails(@RequestBody Customer customer){
         return loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+    }
+
+    @GetMapping("/loan/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(), loansConfig.getMailDetails(),
+                loansConfig.getActiveBranches());
+        return ow.writeValueAsString(properties);
     }
 }
